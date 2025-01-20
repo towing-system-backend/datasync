494dab7835616d9f20dbabec5bbb26c09156084c
Juan <113471575+juanpj17@users.noreply.github.com>
2025-01-20 17:13:07 -0400

    refactor: 🚀 [orders - 005] communication with jobs done

---
+++ datasync/Libs/Core/Src/Infrastructure/Projectors/TowProjector/Models/Mongo.Tow.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Datasync.Core
+{
+    public class MongoTow
+    (
+        string towId,
+        string brand,
+        string model,
+        string color,
+        string licensePlate,
+        string location,
+        int year,
+        string sizeType,
+        string status
+    ) : IEntity
+    {
+        [BsonId]
+        public string TowId = towId;
+        public string Brand = brand;
+        public string Model = model;
+        public string Color = color;
+        public string LicensePlate = licensePlate;
+        public string Location = location;
+        public int Year = year;
+        public string SizeType = sizeType;
+        public string Status = status;
+        public DateTime CreatedAt = DateTime.Now;
+        string IEntity._id => TowId;
+    }
+}
